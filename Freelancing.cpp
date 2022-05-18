7aef6d8d43dd29ae11307ad211f5ba4c7a755086
IT21224348 <99631317+IT21224348@users.noreply.github.com>
2022-05-18 21:25:24 +0530

    Coded by IT21225192

---
+++ Freelancing.cpp
@@ -0,0 +1,35 @@
+#include"FreelancingDesigner.h"
+
+
+   FreelancingDesigner::FreelancingDesigner()
+   {
+   	
+   }
+   FreelancingDesigner::FreelancingDesigner(char Preuname, char Preupassword,int PUID, char PUname[], char PUemail[],int PUcno)
+   :Registereduser( reuname,reupassword,UID,Uname,Uemail,Ucno)
+    {
+    
+    	
+    	
+	}
+    void FreelancingDesigner::placeAproposal(projectProposal * SfreePro)
+    {
+    	
+	}
+	void FreelancingDesigner::submitproject()
+    {
+    	
+	}
+    void FreelancingDesigner::withdrawpayment()
+    {
+    	payments[0]=new payment();
+    	payments[1]=new paymenet();
+	}
+	void FreelancingDesigner::displayDesignerdetails()
+	{
+		
+	}
+	FreelansingDesigner::~FreelancingDesigner()
+	{
+		//Destruction
+	}
