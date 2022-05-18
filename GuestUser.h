f0c558da8df34b79c453a74773d2a35e36d98c73
IT21224348 <99631317+IT21224348@users.noreply.github.com>
2022-05-18 09:23:27 +0000

    Added

---
+++ GuestUser.h
@@ -0,0 +1,15 @@
+#include"AdvertisingPackages.h"
+    class Guestuser
+   {
+       protected:
+          int GuserID;
+          char Gusername[20];
+          char Guseremail[20];
+          int  Guserno;
+        public:
+          Guestuser();
+          Guestuser(int UID, char Uname[], char Uemail[],int Ucno);
+          void searchpackageDetail(AdvetisingPackages *uad);
+          virtual void displayDetail();
+         ~Guestuser();
+   };
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+
 # ooc-project-IT21224348
 ooc-project-IT21224348 created by GitHub Classroom
