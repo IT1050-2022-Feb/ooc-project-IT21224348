aec1835361d123d01e34eb4cf13168336ebe0006
IT21224348 <99631317+IT21224348@users.noreply.github.com>
2022-05-18 21:34:34 +0530

    Coded by IT21226700

---
+++ Report.cpp
@@ -0,0 +1,37 @@
+#include"Report.h"
+
+  
+  Report::Report()
+  {
+  	
+  }
+  Report::Report(payment *pay1[],ProjectProposal *pro1[],ProjectBooking  *book1[])
+  {
+  	
+  	
+  	
+  	
+  	
+  	
+  }
+  void Report::bookingDetail()
+  {
+  	
+  }
+  void Report::proposalDetail()
+  {
+  	
+  }
+  void Report::paymentDetail()
+  {
+  	
+  }
+  Report::~Report()
+  {
+  	
+  }
+  
+  
+  
+  
+  
