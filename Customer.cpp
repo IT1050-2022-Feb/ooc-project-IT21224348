7ae1278abcde98565a2dac0abeab5c0463bfa050
IT21224348 <99631317+IT21224348@users.noreply.github.com>
2022-05-18 21:29:04 +0530

    Coded by IT21227868

---
+++ Customer.cpp
@@ -0,0 +1,46 @@
+#include"Customer.h"
+
+    Customer::Customer()
+    {
+    	
+	}
+    Customer::Customer(char Preuname, char Preupassword,int PUID, char PUname[], char PUemail[],int PUcno)
+   :Registereduser( reuname,reupassword,UID,Uname,Uemail,Ucno)
+   {
+   	
+   }
+   void Customer::searchpackageDetail(AdvertisementPackage *Cpac1)
+   {
+   	
+   }
+   void Customer::palceAproject(ProjectBooking *cBook1)
+   {
+   	
+   }
+   void Customer::selectdesigner(ProjectProposal *cPro1)
+   {
+   	
+   }
+   void Customer::Paypayment()
+   {
+   	    payments[0]=new payment();
+    	payments[1]=new payment();
+   }
+   Customer::~Customer()
+   {
+   	
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
