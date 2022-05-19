35a9b4546ae37f581426f978e206dbdfe57fa69a
IT21224348 <99631317+IT21224348@users.noreply.github.com>
2022-05-19 13:24:53 +0530

    coded by IT21224348

---
+++ Guestuser.cpp
@@ -0,0 +1,30 @@
+#include"Guestuser.h"
+#include<cstring>
+
+Guestuser::Guestuser()
+{
+  GuserID=0;
+  strcpy(Gusername,"");
+  strcpy(Guseremail,"");
+  Guserno=0000000000;
+}
+Guestuser::Guestuser(int UID, char Uname[], char Uemail[],int Ucno)
+{
+    GuserID=UID;
+    strcpy(Gusername,Uname);
+    strcpy(Guseremail,Uemail);
+    Guserno=Ucno;
+}
+void Guestuser::searchpackageDetail(AdvetisementPackage *uad)
+{
+
+}
+void Guestuser::displayDetail()
+{
+  
+}
+Guestuser::~Guestuser()
+{
+  
+}
+
